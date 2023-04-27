21933fe7b00443c487a1ee619188fe72f3214542
razaasif <aasifraza9123@gmail.com>
2023-04-27 23:11:32 +0530

    added warning and success component

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -2,5 +2,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-success-alert',
   templateUrl: './success-alert.component.html',
+  styles: [
+    `
+      p {
+        padding: 20px;
+        background-color: palegreen;
+        border: 1px solid green;
+      }
+    `,
+  ],
 })
 export class SuccessAlertComponent {}
